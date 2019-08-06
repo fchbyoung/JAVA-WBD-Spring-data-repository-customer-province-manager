c578a35d6efbec966823904bffb9f34b6fdb7af2
long <fchbyoung1010@gmail.com>
2019-08-06 08:45:17 +0700

    delete class CustomerRepositoryImpl, interface Repository, edit interface CustomerRepository, extends PagingAndSortingRepository

---
+++ src/main/java/com/codegym/controller/ProvinceController.java
@@ -0,0 +1,12 @@
+package com.codegym.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProvinceController {
+
+
+
+
+}
